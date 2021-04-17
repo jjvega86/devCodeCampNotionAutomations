357909b07ff55252a74cc7e6665a4008225bb371
JJ <jj.vega86@gmail.com>
2021-04-17 06:10:18 -0400

    Init

---
+++ main.py
@@ -0,0 +1,5 @@
+# Main entry point of application
+from automations import activestudents
+
+activestudents.change_standup_status_notstarted('Part-Time')
+activestudents.change_standup_status_notstarted('Full-Time')
