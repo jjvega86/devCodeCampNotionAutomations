e0a017bbd2eec756f8fc467b6a4b1a93e7127fd5
JJ <jj.vega86@gmail.com>
2021-04-18 16:37:10 -0400

    Finished functions to get datetime objects for all days school is closed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Main entry point of application
+from datetime import date
 from automations import activestudents, classschedules
+from utilities import datetimetools
 
 '''
 # Change standup status to Not Started
@@ -7,4 +9,6 @@ activestudents.change_standup_status_notstarted('Part-Time')
 activestudents.change_standup_status_notstarted('Full-Time')
 '''
 
-print(classschedules.get_schedule_to_add_dates_to())
+#   print(classschedules.get_schedule_to_add_dates_to())
+
+datetimetools.get_school_closed_datetime_objects()
--- utilities/datetimetools.py
+++ utilities/datetimetools.py
@@ -1,10 +1,54 @@
 from notion.collection import NotionDate
 from utilities import user_interface
 from datetime import datetime, timedelta
+import holidays
+
+dates_school_closed = [
+    # This list is used to filter current holidays based off of the current year
+    "New Year's Day",
+    "Memorial Day",
+    "Independence Day",
+    "Labor Day",
+    "Thanksgiving",
+    "Christmas Day"
+]
 
 
 # TODO: add holiday parsing logic to change course end date based on dates the school is closed outside of weekends
+# TODO: get dates the school is closed on Christmas week based on Christmas Day
 # https://pypi.org/project/holidays/ -- Use this library to create custom holiday object to test date for class/no class
+
+def get_school_closed_datetime_objects():
+    current_year = datetime.now().strftime('%Y')  # Get current year
+    current_holidays = holidays.UnitedStates(years=int(current_year))  # Get current year's holidays
+    filtered_holidays = []
+    for date in current_holidays.items():  # each item in current_holidays is a tuple, with the date at the 0 index and name at the 1 index
+        if date[1] == "Christmas Day":
+            filtered_holidays.append(date[0])
+            filtered_holidays = add_chrismas_week_days(filtered_holidays, date[0])
+        elif date[1] in dates_school_closed:
+            filtered_holidays.append(date[0])
+    return filtered_holidays
+
+
+def add_chrismas_week_days(filtered_holidays, christmas_day):
+    if christmas_day.weekday() == 0:
+        dates = [christmas_day+timedelta(days=1), christmas_day+timedelta(days=2),christmas_day+timedelta(days=3), christmas_day+timedelta(days=4)]
+    elif christmas_day.weekday() == 1:
+        dates = [christmas_day - timedelta(days=1), christmas_day + timedelta(days=1), christmas_day + timedelta(days=2), christmas_day + timedelta(days=3)]
+    elif christmas_day.weekday() == 2:
+        dates = [christmas_day - timedelta(days=2), christmas_day - timedelta(days=1), christmas_day + timedelta(days=1), christmas_day + timedelta(days=2)]
+    elif christmas_day.weekday() == 3:
+        dates = [christmas_day - timedelta(days=3), christmas_day - timedelta(days=2), christmas_day - timedelta(days=1), christmas_day + timedelta(days=1)]
+    elif christmas_day.weekday() == 4:
+        dates = [christmas_day - timedelta(days=4), christmas_day - timedelta(days=3), christmas_day - timedelta(days=2), christmas_day - timedelta(days=1)]
+    elif christmas_day.weekday() == 5:
+        dates = [christmas_day - timedelta(days=5), christmas_day - timedelta(days=4), christmas_day - timedelta(days=3), christmas_day - timedelta(days=2), christmas_day - timedelta(days=1)]
+    elif christmas_day.weekday() == 6:
+        dates = [christmas_day + timedelta(days=1), christmas_day + timedelta(days=2), christmas_day + timedelta(days=3), christmas_day + timedelta(days=4), christmas_day + timedelta(days=5)]
+
+    combined_dates = dates + filtered_holidays
+    return combined_dates
 
 
 def create_cohort_date(course_type):
